62c4bc58b6f524901d1924ffd64beca9ce72dfb9
Facundo Sosa <Facundososa98@hotmail.com>
2025-10-14 20:45:28 -0300

    Version final para la pruebas

--- Miprimerapaginafsosa/settings.py
+++ Miprimerapaginafsosa/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "dev-only"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Esto permite TODAS las conexiones
 
 INSTALLED_APPS = [
     "django.contrib.admin",

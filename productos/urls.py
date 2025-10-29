c4886cfaac45fbcabb150b48b45dfdf63a6288e1
Facundo Sosa <Facundososa98@hotmail.com>
2025-10-28 21:18:10 -0300

    Add test URL for product image

--- productos/urls.py
+++ productos/urls.py
@@ -4,3 +4,9 @@ from .views import catalogo
 urlpatterns = [
     path('catalogo/', catalogo, name='catalogo'),
 ]
+
+from .views import test_imagen_url
+
+urlpatterns += [
+    path('test-imagen/', test_imagen_url),
+]

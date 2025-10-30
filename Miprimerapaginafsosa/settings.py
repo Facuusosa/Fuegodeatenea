bc683a857eb2ed7b3e12fe9f9dad15dcf8e9aa56
Facundo Sosa <Facundososa98@hotmail.com>
2025-10-29 22:06:06 -0300

    Activate Cloudinary for media files

--- Miprimerapaginafsosa/settings.py
+++ Miprimerapaginafsosa/settings.py
@@ -14,8 +14,8 @@ else:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = [
-    # 'cloudinary_storage',  # Comentado temporalmente
-    # 'cloudinary',  # Comentado temporalmente
+    'cloudinary_storage',
+    'cloudinary',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -116,7 +116,7 @@ import cloudinary.uploader
 import cloudinary.api
 
 cloudinary.config(**CLOUDINARY_STORAGE)
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'  # Comentado temporalmente
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Seguridad para producción
 if not DEBUG:
